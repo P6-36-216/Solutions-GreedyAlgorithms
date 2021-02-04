2e3d26a078900527b53273e4e737e6e6d6d29185
Pramatha <59904736+Pramatha-V@users.noreply.github.com>
2021-02-04 20:12:37 +0530

    Update Grid-Challenge.py

--- Grid-Challenge.py
+++ Grid-Challenge.py
@@ -1,36 +1,25 @@
-#!/bin/python3
+#!/bin/python
 
-import math
-import os
-import random
-import re
 import sys
 
-# Complete the gridChallenge function below.
 def gridChallenge(grid):
     for i in range(n):
         grid[i] = sorted(grid[i])
     for i in range(n-1):
-        for j in range(n-1):
+        for j in range(len(grid[i])):
             if grid[i][j]>grid[i+1][j]:
                 return "NO"
-    return "YES" 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    return "YES"        
+        
 
-    t = int(input())
-
-    for t_itr in range(t):
-        n = int(input())
-
+if __name__ == "__main__":
+    t = int(input().strip())
+    for _ in range(t):
+        n = int(input().strip())
         grid = []
+        grid_i = 0
+        for grid_i in range(n):
+            grid_t = str(input().strip())
+            grid.append(grid_t)
+        print(gridChallenge(grid))
 
-        for _ in range(n):
-            grid_item = input()
-            grid.append(grid_item)
-
-        result = gridChallenge(grid)
-
-        fptr.write(result + '\n')
-
-    fptr.close()
